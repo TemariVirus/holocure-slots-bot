82bd68731d4d8858248944c4ebd6c3a2061dcca0
TEMARI VÍRUS <kiyonnxperiaz@gmail.com>
2024-11-18 20:32:20 +0800

    Remove unncessary usings

--- Program.cs
+++ Program.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
-using System.Threading;
 
 // TODO: Check out OBS windows 10 capture method
 
